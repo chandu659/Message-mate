5831951a3bce67784eafb5874f83a393ac24c7a3
chandu-reddy247 <chandramohan@gamyam.io>
2024-11-04 12:08:15 -0600

    Secure backend connection

--- Frontend/zimmer-chat/src/lib/pocketbase.js
+++ Frontend/zimmer-chat/src/lib/pocketbase.js
@@ -1,7 +1,7 @@
 import PocketBase from 'pocketbase';
 import { writable } from 'svelte/store';
 
-export const pb = new PocketBase('http://66.55.65.128');
+export const pb = new PocketBase('https://pocketbase-api.webs.vc');
 
 export const currentUser = writable(pb.authStore.model);
 
